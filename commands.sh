4d8451848ca529531eaaedcdc7433d6b86c5c166
chaima arfaoui <chaima.arfaoui.ext@orange.com>
2020-12-11 12:13:52 +0100

    Initial commit

---
+++ commands.sh
@@ -0,0 +1,11 @@
+Projet integration vault concourse ---> edit value.yaml, value.yaml of chart helm
+
+scp -i masterKey.pem ubuntu@15.188.144.60:~/.kube/config /home/arfaoui/aws
+export KUBECONFIG=~/.kube/aws-cluster-config
+kubectl config view
+Problem : The authenticity of host 'ec2' can't be established.
+ssh -o "StrictHostKeyChecking no" user@host
+Problem : Got permission denied while trying to connect to the Docker daemon :
+Add your user to the docker group
+$sudo usermod -aG docker ${USER}
+
